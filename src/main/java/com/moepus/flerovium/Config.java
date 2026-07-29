12aa0c2232814175eb4dac907f9cd9997a398444
SourceNa₂CO₃Zn <125875649+safesaffawa@users.noreply.github.com>
2026-07-30 07:15:55 +0800

    new

--- src/main/java/com/moepus/flerovium/Config.java
+++
@@ -1,8 +0,0 @@
-package com.moepus.flerovium;
-
-public class Config {
-    public boolean entityBackFaceCulling = true;
-    public boolean itemBackFaceCulling = true;
-    public boolean reduceTerrainParticles = true;
-    public boolean disableSoundDistanceCull = false;
-}
