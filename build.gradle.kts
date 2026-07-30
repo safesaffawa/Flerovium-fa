31587d20e0827210c7e326d03f56bb0866c10206
SourceNa₂CO₃Zn <125875649+safesaffawa@users.noreply.github.com>
2026-07-30 13:56:31 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,20 @@
+import net.fabricmc.loom.api.LoomGradleExtension
+
 plugins {
-    id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT"
+    id("fabric-loom") version "1.17-SNAPSHOT"
     id("maven-publish")
+    id("java")
 }
 
 version = project.property("mod_version") as String
 group = project.property("maven_group") as String
 
 repositories {
-    // Add repositories to retrieve artifacts from in here.
-    // You should only use this when depending on other mods because
-    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
-    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
-    // for more information about repositories.
+    maven {
+        name = "Modrinth"
+        url = uri("https://api.modrinth.com/maven")
+    }
+    mavenCentral()
 }
 
 loom {
@@ -26,12 +29,8 @@ loom {
 }
 
 dependencies {
-    // Minecraft
     minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
-    
-    // Fabric API
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
-
     modCompileOnly("maven.modrinth:sodium:2Yom1N68")
     modCompileOnly("maven.modrinth:iris:oaD6KQls")
 }
@@ -39,48 +38,31 @@ dependencies {
 tasks.processResources {
     val version = project.version.toString()
     inputs.property("version", version)
-
     filesMatching("fabric.mod.json") {
         expand(mapOf("version" to version))
     }
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.release.set(25)
+    options.release.set(21)
 }
 
 java {
-    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
-    // if it is present.
-    // If you remove this line, sources will not be generated.
     withSourcesJar()
-
-    sourceCompatibility = JavaVersion.VERSION_25
-    targetCompatibility = JavaVersion.VERSION_25
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.jar {
-    val projectName = project.name
-    inputs.property("projectName", projectName)
-
     from("LICENSE") {
-        rename { "${it}_$projectName" }
+        rename { "${it}_${project.name}" }
     }
 }
 
-// configure the maven publication
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
         }
-    }
-
-    // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
-    repositories {
-        // Add repositories to publish to here.
-        // Notice: This block does NOT have the same function as the block in the top level.
-        // The repositories here will be used for publishing your artifact, not for
-        // retrieving dependencies.
     }
 }
