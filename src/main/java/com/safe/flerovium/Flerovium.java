39bacc4ed01c2b723508dfc9b44a3d52f8263e6c
SourceNa₂CO₃Zn <125875649+safesaffawa@users.noreply.github.com>
2026-07-31 16:03:16 +0800

    new stars2

--- src/main/java/com/safe/flerovium/Flerovium.java
+++ src/main/java/com/safe/flerovium/Flerovium.java
@@ -1,7 +1,7 @@
 package com.safe.flerovium;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+
 import net.minecraft.resources.Identifier;
 
 import org.slf4j.Logger;
@@ -9,19 +9,22 @@ import org.slf4j.LoggerFactory;
 
 public class Flerovium implements ModInitializer {
 	public static final String MOD_ID = "flerovium";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final Config config = ConfigParser.getConfig();
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Flerovium initialized! Making rendering faster...");
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
 	}
 
 	public static Identifier id(String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
-	}
-
-	public static boolean isModLoaded(String modId) {
-		return FabricLoader.getInstance().isModLoaded(modId);
 	}
 }
