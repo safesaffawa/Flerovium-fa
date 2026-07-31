030b5f845ae039741ac4beee9aff8f727fdf5499
SourceNa₂CO₃Zn <125875649+safesaffawa@users.noreply.github.com>
2026-07-31 10:27:56 +0800

    new stars

--- src/client/java/safe/flerovium/client/FleroviumClient.java
+++
@@ -1,9 +0,0 @@
-package safe.flerovium;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class FleroviumClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
--- src/main/java/safe/flerovium/Config.java
+++
@@ -1,8 +0,0 @@
-package safe.flerovium;
-
-public class Config {
-    public boolean entityBackFaceCulling = true;
-    public boolean reduceTerrainParticles = true;
-    public boolean skipEntityTangentCompute = false;
-    public boolean fastBreakingTexture = true;
-}
